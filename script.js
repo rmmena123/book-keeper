ed736095af7fafd20a43030d0b17ba38b5f7c62e
Rodrigo Machado Mena <rodrigo_mena_corebiz@whirlpool.com>
2022-11-14 11:13:15 -0300

    #6 - Validação do formulário

--- script.js
+++ script.js
@@ -14,3 +14,38 @@ function showModal() {
 modalShow.addEventListener('click', showModal);
 modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
 window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
+
+function validateForm(nameValue, urlValue) {
+    const expression = /(https)?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/g;
+    const regex = new RegExp(expression);
+
+    if (!nameValue || !urlValue) {
+        alert('Please submit values for both fields.');
+        return false;
+    }
+
+    if(!urlValue.match(regex)) {
+        alert('Please provide a valid web address.');
+        return false;
+    }
+
+    return true;
+}
+
+function storeBookmark(e) {
+    e.preventDefault();
+    const nameValue = websiteNameEl.value;
+    let urlValue = websiteUrlEl.value;
+    
+    if (!urlValue.includes('https://') && !urlValue.includes('http://')) {
+     urlValue = `https://${urlValue}`; 
+    }
+
+    if(!validateForm(nameValue, urlValue)) {
+        return false;
+    }
+
+    validateForm(nameValue, urlValue);
+}
+
+bookmarkForm.addEventListener('submit', storeBookmark);
