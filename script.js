e472a152acdbb87d30784ad761d8d95232a57f41
Rodrigo Machado Mena <rodrigo_mena_corebiz@whirlpool.com>
2022-11-14 10:42:24 -0300

    #5 - Abrir e fechar modal

---
+++ script.js
@@ -0,0 +1,16 @@
+const modal = document.getElementById('modal');
+const modalShow = document.getElementById('show-modal');
+const modalClose = document.getElementById('close-modal');
+const bookmarkForm = document.getElementById('bookmark-form');
+const websiteNameEl = document.getElementById('website-name');
+const websiteUrlEl = document.getElementById('website-url');
+const bookmarksContainer = document.getElementById('bookmarks-container');
+
+function showModal() {
+    modal.classList.add('show-modal');
+    websiteNameEl.focus();
+}
+
+modalShow.addEventListener('click', showModal);
+modalClose.addEventListener('click', () => modal.classList.remove('show-modal'));
+window.addEventListener('click', (e) => (e.target === modal ? modal.classList.remove('show-modal') : false));
